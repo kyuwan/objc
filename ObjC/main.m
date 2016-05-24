e2bb53bd76181fa4e756fb501bdd1d8a3740fab3
kyuwan jang <dong10719@gmail.com>
2016-05-24 11:03:39 +0900

    Initial Commit

---
+++ ObjC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ObjC
+//
+//  Created by MF839-005 on 2016. 5. 24..
+//  Copyright © 2016년 Sample. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
